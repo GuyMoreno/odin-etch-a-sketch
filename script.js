dc21c736439026f7f3de5d5ec6177fdaae2753f2
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-19 13:40:52 +0200

    finished working prompt and new grid button

--- script.js
+++ script.js
@@ -14,16 +14,33 @@ function createGrid(size) {
         for (let col = 0; col < size; col++) {
             const square = document.createElement('div');
             square.classList.add('square');
-
             // הגדרת גודל הריבוע בצורה דינמית
             square.style.width = `${squareSize}px`;
             square.style.height = `${squareSize}px`;
-
             container.appendChild(square);
 
+            // hovering effect dinamically
+            square.addEventListener('mouseenter', () => {
+                // changing color
+                square.style.backgroundColor = 'darkblue';
+            });
         }
     }
 }
 
 // Initially create a 16x16 grid
+
+
+document.getElementById('new-grid').addEventListener('click', () => {
+    // prompting for a size
+    const num = parseInt(prompt('What is the size? '));
+    // check whether input valid
+    if (!isNaN(num) && num > 0 && num < 100) {
+        createGrid(num); // יוצרים את הגריד החדש
+    } else {
+        alert('Please enter a valid number');
+    }
+});
+
+
 createGrid(16);
