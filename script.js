28f996812ce45a3c8a7d7b578051d124f0095ad4
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2024-11-20 11:43:32 +0200

    tidy the code and preetify the button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// get the element to work with here
 const container = document.getElementById('grid-container');
 // Function to create the grid
 function createGrid(size) {
-    container.innerHTML = ''; // Clear any existing grid
-    // השורה להלן מאחסנת משתנה ובו הרוחב של המיכל
+    // clear the previous grid
+    container.innerHTML = '';
+    // new var that stores the div's width
     const containerWidth = container.offsetWidth;
+    // calc sqaure size in accordance to div size
     const squareSize = containerWidth / size;
-    // חישוב גודל הריבוע (בהתאם למספר הריבועים בשורה)
     // Create the squares
     for (let row = 0; row < size; row++) {
         for (let col = 0; col < size; col++) {
@@ -15,7 +17,6 @@ function createGrid(size) {
             square.style.width = `${squareSize}px`;
             square.style.height = `${squareSize}px`;
             container.appendChild(square);
-
             // hovering effect dinamically
             square.addEventListener('mouseenter', () => {
                 // changing color
@@ -25,12 +26,14 @@ function createGrid(size) {
         }
     }
 }
-
+// get grandom color function logic
 function getRandomColor() {
-    // כל האפשרויות התקפות בתווי צבע
+    // an array of hex chars
     const chars = '0123456789ABCDEF';
+    // prefix # 
     let color = '#'
     for (let i = 0; i < 6; i++) {
+        // the var starts with the prefix...
         color += chars[Math.floor(Math.random() * 16)];
     }
     return color;
@@ -47,6 +50,4 @@ document.getElementById('new-grid').addEventListener('click', () => {
         alert('Please enter a valid number');
     }
 });
-
-
 createGrid(16);
